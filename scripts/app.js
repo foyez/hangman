5cc5a58b9afe8de1518e250afc22aa9e3e4ce375
Foyez <foyezar@gmail.com>
2018-10-24 02:18:39 +0600

    initial commit

---
+++ scripts/app.js
@@ -0,0 +1,31 @@
+const puzzleEl = document.querySelector('#puzzle')
+const guessesMessage = document.querySelector('#guesses-message')
+let game1
+
+window.addEventListener('keypress', (e) => {
+  const guess = String.fromCharCode(e.charCode)
+  if(game1.status === 'playing') {
+    game1.makeGuess(guess)
+    render()
+  }
+})
+
+const render = () => {
+  puzzleEl.innerHTML = ''
+  guessesMessage.textContent = game1.statusMessage
+
+  game1.puzzle.split('').forEach(letter => {
+    const letterEl = document.createElement('span')
+    letterEl.textContent = letter
+    puzzleEl.appendChild(letterEl)
+  })
+}
+
+const startGame = async () => {
+  const puzzle = await getPuzzle('2')
+  game1 = new Hangman(puzzle, 5)
+  render()
+}
+
+document.querySelector('#reset').addEventListener('click', startGame)
+startGame()
